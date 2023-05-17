38463dc47b5a3a498c66c85225975879eaf1c5da
Caroline <carolinefernandesc@hotmail.com>
2023-05-17 17:38:24 -0300

    Requisito 03

--- src/route/loginRoute.js
+++ src/route/loginRoute.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
-// const fs = require('fs').promises;
+const generateToken = require('../utils/generateToken');
+const auth = require('../middlewares/auth');
 
 const routerLogin = express.Router();
+
+routerLogin.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  if ([email, password].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+})
 
 module.exports = routerLogin;
