7185a666e69ce6ca999cad6c7446e205f76555ad
Caroline <carolinefernandesc@hotmail.com>
2023-05-17 16:39:47 -0300

    corrigindo lint

--- src/route/talkerRoute.js
+++ src/route/talkerRoute.js
@@ -6,18 +6,20 @@ const router = express.Router();
 
 const path = require('path');
 
-router.get('/',async (req,res) => {
-  const data = await fs.readFile(path.resolve(__dirname,'../talker.json'), 'utf-8');
+router.get('/',async (_req,res) => {
+  const data = await fs.readFile(path.resolve(__dirname,'../talker.json'),'utf-8');
   const result = JSON.parse(data);
   return res.status(200).send(result);
 });
 
 router.get('/:id',async (req,res) => {
   const { id } = req.params;
-  const data = await fs.readFile(path.resolve(__dirname,'../talker.json'), 'utf-8');
+  const data = await fs.readFile(path.resolve(__dirname,'../talker.json'),'utf-8');
   const result = JSON.parse(data);
   const findResult =  result.find((element) => Number(element.id) === Number(id));
-  if (findResult === undefined) res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
+  if (findResult === undefined) {
+    return res.status(404).json({ 'message': 'Pessoa palestrante não encontrada' });
+  };
   return res.status(200).send(findResult);
 });
 
