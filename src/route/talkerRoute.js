c9c2904c2ff7e6bd8b9eb6986761ffbf9046d8d7
Caroline <carolinefernandesc@hotmail.com>
2023-05-17 16:34:05 -0300

    Requisito 02

--- src/route/talkerRoute.js
+++ src/route/talkerRoute.js
@@ -12,4 +12,13 @@ router.get('/', async (req,res) => {
   return res.status(200).send(result);
 });
 
+router.get('/:id', async (req,res) => {
+  const { id } = req.params;
+  const data = await fs.readFile(path.resolve(__dirname,'../talker.json'), 'utf-8');
+  const result = JSON.parse(data);
+  const findResult =  result.find((element) => Number(element.id) === Number(id));
+  if (findResult === undefined) res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
+  return res.status(200).send(findResult);
+});
+
 module.exports = router;
