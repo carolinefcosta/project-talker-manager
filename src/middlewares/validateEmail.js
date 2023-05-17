0e42fe232a1476ab4f8ecb178a5e859e3d2ae493
Caroline <carolinefernandesc@hotmail.com>
2023-05-17 18:24:27 -0300

    Requisito 04

--- src/middlewares/validateEmail.js
+++ src/middlewares/validateEmail.js
@@ -1,19 +1,19 @@
 const validateEmail = (req, res, next) => {
-  const email = req.body.email;
+  const { email } = req.body;
 
   const regex = /\S+@\S+\.\S+/;
 
   if (!email) {
     return res.status(400).json({
-      message: 'O campo \"email\" é obrigatório',
+      message: 'O campo "email" é obrigatório',
     });
   }
   if (!regex.test(email)) {
     return res.status(400).json({
-      message: 'O \"email\" deve ter o formato \"email@email.com\"',  
+      message: 'O "email" deve ter o formato "email@email.com"',  
     });
   }
   next();
-}
+};
 
 module.exports = validateEmail;
